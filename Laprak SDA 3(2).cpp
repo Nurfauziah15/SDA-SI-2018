851745aa8ff35506de6d1803fd844f7cc6a41586
Nurfauziah15 <48704073+Nurfauziah15@users.noreply.github.com>
2019-04-08 17:16:26 +0700

    Add files via upload

---
+++ Laprak SDA 3(2).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void quicksort (int data[], int kiri, int kana){
+	int i = kiri;
+	int j = kanan;
+	int temp;
+	int pivot =  [(kiri + kanan)/2]
+	
+	while (i <= j){
+		while (data [i] < pivot){
+			i++;
+		}
+		while (data [j] > pivot){
+			j--;
+		}
+		if (i <= j){
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp
+			i++;
+			j--;
+		}
+	}
+	if (kiri < j){
+		quicksort ( data, kiri, j);	
+	}
+	if (i < kanan){
+		quicksort ( data, i, kanan);	
+	}
+}
+
+int main (){
+	int data [5] = {3,1,4,2,5};
+				// 0 1 2 3 4
+				
+	
+	
+	quicksort (data, 0, 4);
+}
